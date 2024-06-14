bf176b1fea36848dcf9106cc099c14c37d7eb430
Giovani <giakomogcs@gmail.com>
2024-06-13 21:34:47 -0300

    adicionando fontFamily={'heading'}

--- src/components/HistoryCard.tsx
+++ src/components/HistoryCard.tsx
@@ -17,6 +17,7 @@ export function HistoryCard() {
           fontSize={"md"}
           textTransform={"capitalize"}
           numberOfLines={1}
+          fontFamily={"heading"}
         >
           Costas
         </Heading>
