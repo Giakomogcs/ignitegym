0734b376a8d0c9033c6640517bbede5eb16a33e2
Giovani <giakomogcs@gmail.com>
2024-06-19 20:57:41 -0300

    Cadastrando usuário e inplementando AppError

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,5 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 
 import { useForm, Controller } from "react-hook-form";
 import * as yup from "yup";
@@ -10,6 +18,11 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { api } from "@services/api";
+
+import axios from "axios";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -28,10 +41,11 @@ const signUpSchema = yup.object({
   password_confirm: yup
     .string()
     .required("Confirme a senha.")
-    .oneOf([yup.ref("password")], "A comfirmação da senha não confere"),
+    .oneOf([yup.ref("password")], "A confirmação da senha não confere"),
 });
 
 export function SignUp() {
+  const toast = useToast();
   const navigation = useNavigation();
   const {
     control,
@@ -45,8 +59,22 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: any) {
-    console.log(data);
+  async function handleSignUp({ name, email, password }: FormDataProps) {
+    try {
+      const response = await api.post("/users", { name, email, password });
+
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível criar a conta. Tente novamente mais tarde.";
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
