e2c64f967f6a524d2026cd8ff715ac01234b775d
Giovani <giakomogcs@gmail.com>
2024-07-18 21:49:24 -0300

    Exibindo o avatar atualizado

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -25,6 +25,8 @@ import { useAuth } from "@hooks/useAuth";
 import { api } from "@services/api";
 import { AppError } from "@utils/AppError";
 
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
+
 const PHOTO_SIZE = 33;
 
 type FormDataProps = {
@@ -60,9 +62,6 @@ const profileSchema = yup.object({
 export function Profile() {
   const [isUpdating, setIsUpdating] = useState(false);
   const [photoIsLoading, setPhotoIsLoagind] = useState(false);
-  const [userPhoto, setUserPhoto] = useState(
-    "https://github.com/Giakomogcs.png"
-  );
 
   const { user, updateUserProfile } = useAuth();
 
@@ -118,11 +117,21 @@ export function Profile() {
 
         const userPhotoUploadForm = new FormData();
         userPhotoUploadForm.append("avatar", photoFile);
-        await api.patch("/users/avatar", userPhotoUploadForm, {
-          headers: {
-            "Content-Type": "multipar/form-data",
-          },
-        });
+
+        const avatarUpdatedResponse = await api.patch(
+          "/users/avatar",
+          userPhotoUploadForm,
+          {
+            headers: {
+              "Content-Type": "multipar/form-data",
+            },
+          }
+        );
+
+        const userUpdated = user;
+        userUpdated.avatar = avatarUpdatedResponse.data.avatar;
+        updateUserProfile(userUpdated);
+
         toast.show({
           title: "Foto atualizada!",
           placement: "top",
@@ -182,7 +191,11 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: userPhoto }}
+              source={
+                user.avatar
+                  ? { uri: `${api.defaults.baseURL}/avatar/${user.avatar}` }
+                  : defaultUserPhotoImg
+              }
               alt="Foto do usuario"
               size={PHOTO_SIZE}
             />
